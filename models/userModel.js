e0ca8b529af8b66f57ccb40e84f646cf3e95afd4
Chandra Prakash <78693357+Chandra380@users.noreply.github.com>
2023-12-19 23:58:32 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -9,6 +9,7 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       required: [true, "email is required"],
+      unique: tue,
     },
     password: {
       type: String,
